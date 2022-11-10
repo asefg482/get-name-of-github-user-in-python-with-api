c08a8960db3efd6858c1f1e4524451c3b8c04abd
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2022-11-10 01:27:35 -0500

    Updated code formatting into modular functions
    Moved the code into a function so we can run unittests on it if need be. Plus just making the format a bit more conventional for python standards. :D

--- get-name-of-user-github-python.py
+++ get-name-of-user-github-python.py
@@ -1,4 +1,29 @@
 import requests
-API_URL="https://api.github.com/users/asefg482"
-Response = requests.get(API_URL)
-print("Name : "+Response.json()['name'])
+
+def get_name(username: str):
+    """
+    This function takes a GitHub username and outputs the GitHub display name.
+
+    :param username: Any valid GitHub username
+    :returns: display name as a string variable.
+    """
+
+    API_URL = f"https://api.github.com/users/{username}"
+    Response = requests.get(API_URL)
+    display_name = str(Response.json()['name'])
+    formatted_name_string = f"Name: {display_name}"
+    print(formatted_name_string)
+
+    return display_name
+
+
+def main():
+    """
+    Demonstrating how to use the get_name() function.
+    """
+
+    get_name("asefg482")
+
+
+if __name__ == "__main__":
+    main()
